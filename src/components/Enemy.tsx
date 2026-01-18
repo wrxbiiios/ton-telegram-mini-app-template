793623620938c2f1df8287d5b2170146243a51b3
wrxbiiios <rxbiiio24@gmail.com>
2026-01-18 02:46:25 -0800

    Add Enemy component

---
+++ src/components/Enemy.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Enemy: React.FC = () => {
+    return (
+        <div>
+            <h1>Enemy Component</h1>
+        </div>
+    );
+};
+
+export default Enemy;
